0b4ca4268827f5b55e6fde269b50765defa2eb59
butch888 <never_man@mail.ru>
2024-10-08 23:00:34 +0300

    fix

--- app/page.js
+++ app/page.js
@@ -3,22 +3,18 @@ import Posts from "./components/Posts";
 const URL2 = 'http://localhost:3000/api/data'
 
 async function FetchData() {
-  const res = await fetch(URL2)
-  const result = await res.json()
-  return result
-}
+  const res = await fetch(URL2);
+  const result = await res.json();
+  return result;
+};
 
 
 export default async function Home() {
-  const posts = await FetchData()
-
-  console.log(posts)
+  const posts = await FetchData();
 
   return (
     <div>
       <Posts posts={posts} />
-
     </div>
-
   );
-}
+};
